97aec715c9a2d9aa7a8b49f886bed1344a72384c
antuelo <antuel@outlook.com>
2025-01-28 00:56:37 +0200

    me voy a dormir, ft printf no funciona

--- src/main.c
+++ src/main.c
@@ -6,29 +6,44 @@
 /*   By: anoviedo <antuel@outlook.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 14:26:27 by anoviedo          #+#    #+#             */
-/*   Updated: 2025/01/27 22:54:03 by anoviedo         ###   ########.fr       */
+/*   Updated: 2025/01/28 00:55:46 by anoviedo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include "../include/push_swap.h"
+#include "../ft_printf/libftprintf.h"
 
 int	main(void)
 {
 	t_stack		stack;
 	t_node		*current;
+	int			value;
 
 	create_stack(&stack);
 	push(&stack, 10);
 	push(&stack, 20);
 	push(&stack, 30);
 	current = stack.top;
-	printf("contenido de la pila:\n");
+	ft_printf("contenido de la pila antes de pop:\n");
 	while (current)
 	{
 		printf("%d\n", current->value);
 		current = current->next;
 	}
+	value = pop(&stack);
+	current = stack.top;
+	while (current)
+	{
+		ft_printf("%d\n", current->value);
+		current = current->next;
+	}
+	ft_printf("el valor eliminado es: %d\n", value);
 	return (0);
 }
+
+/*
+gcc -Wall -Wextra -Werror src/main.c src/stack_utils.c
+ src/stack_operations.c -o push_swap
+*/
